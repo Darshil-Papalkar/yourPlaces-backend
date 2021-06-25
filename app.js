2bace17aaa0b32c04fb4fc7414540941070dfaad
Darshil-Papalkar <papalkardarshil13@gmail.com>
2021-06-26 02:57:14 +0530

    added port

--- app.js
+++ app.js
@@ -49,8 +49,8 @@ app.use((error, req, res, next) => {
 mongoose
     .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.egg5y.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {useUnifiedTopology: true, useNewUrlParser: true})
     .then(() => {
-            app.listen(5000, () => {
-                console.log("Server started on port 5000");  
+            app.listen(process.env.PORT || 5000, () => {
+                console.log("Server started");  
             });
     })
     .catch(err => {
